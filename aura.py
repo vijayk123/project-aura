a39d3dfba90eaa44588ac10a98666992dac6c18b
vijayk123 <vijaykumar.vartha@gmail.com>
2026-02-04 06:56:19 +0530

    New version again

--- aura.py
+++ aura.py
@@ -12,7 +12,7 @@ def home():
 def health():
       return jsonify({
         "status": "UP",
-        "version": "v4"
+        "version": "v4, elocome to the updated version"
     })
 @app.route("/region")
 def region():
